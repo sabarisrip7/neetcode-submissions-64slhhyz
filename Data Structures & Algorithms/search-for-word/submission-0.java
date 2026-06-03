e11eaf324b5883a263eae710523ad11549cffb60
Sabari sri p <sabaripremkumar3@gmail.com>
2026-06-03 10:16:38 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(search(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean search(char[][] board,String word,int row,int col,int index){
+        if(index == word.length()){
+            return true;
+        }
+        if(row<0 || col<0 || row>=board.length || col>=board[0].length || board[row][col] != word.charAt(index)){
+            return false;
+        }
+
+        char temp = board[row][col];
+        boolean found = search(board, word, row+1, col, index+1) || search(board,word,row-1,col,index+1) || search(board,word,row,col+1,index+1) || search(board,word,row,col-1,index+1);
+
+        board[row][col] = temp;
+        return found;
+    }
+}
